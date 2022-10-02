c86d171bedbf839488ae29cdc3e89b37dacc052b
Luis Bautista Machuca <luiscarlosbautistamachuca@gmail.com>
2022-10-02 13:47:17 -0500

    Slider Custom

---
+++ src/slider/JsliderCustom.java
@@ -0,0 +1,20 @@
+
+package slider;
+
+import java.awt.Color;
+import javax.swing.JSlider;
+
+/**
+ *
+ * @author MenDark
+ */
+public class JsliderCustom extends JSlider {
+    
+    public JsliderCustom(){
+        setOpaque(false);
+        setBackground(new Color(180, 180, 180));
+        setForeground(new Color(69, 124, 235));
+        setUI(new JSliderUI(this));
+    }
+    
+}
